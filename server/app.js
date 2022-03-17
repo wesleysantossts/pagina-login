fdc1f5c9363526b2333839a669ae49754a9ed6d8
Wesley <wesley.brazil@outlook.com>
2022-03-17 12:23:09 -0300

    Main: bd conectado

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
-const express = require("express"), cors = require("cors"), routes = require("./routes");
+const express = require("express"), cors = require("cors"), routes = require("./routes"), mongoose = require("mongoose");
+require("dotenv").config();
+const MONGO = process.env.MONGO;
 
 class App {
   constructor(){
     this.server = express();
+    mongoose.connect(MONGO);
 
     this.middlewares();
     this.routes();
---
+++ server/controller/SessionController.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+class SessionController {
+  BuscarId(req, res){
+
+  }
+}
+
+module.exports = new SessionController();
