9fafc7e648bc9534e88ee503459b1034b550551d
Wesley <wesley.brazil@outlook.com>
2022-03-22 16:18:36 -0300

    Ft/controller: configuração de sessão ok

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,12 +2,16 @@ import {Switch} from "react-router-dom";
 import MyRoute from "./MyRoute";
 
 import Signup from "../view/Signup";
+import Signin from "../view/Signin";
+import Home from "../view/Home";
 import Page404 from "../view/Page404";
 
 export default function Routes() {
   return (
     <Switch>
       <MyRoute path="/signup" component={Signup} /*isClosed*/ />
+      <MyRoute path="/signin" component={Signin} /*isClosed*/ />
+      <MyRoute path="/home" component={Home} isClosed />
       <MyRoute path="*" component={Page404}/>
     </Switch>
   );
