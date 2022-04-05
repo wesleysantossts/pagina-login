90205092132f8c3403bbf5029e91b5163be54664
Wesley <wesley.brazil@outlook.com>
2022-04-05 14:57:27 -0300

    Pagina signup ok

--- client/src/view/Signup/index.js
+++ client/src/view/Signup/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-
 import api from "../../service/api";
+import "./signup.css"
 
 export default function Signup(){
   const [nome, setNome] = useState("")
@@ -32,19 +32,17 @@ export default function Signup(){
   
 
   return(
-    <div>
-      <h1>Cadastre-se</h1>
-      
-      <input type="text" name="nome" className="nome" placeholder="Insira seu nome" value={nome} onChange={(e)=> setNome(e.target.value)} /><br/>
-      <input type="email" name="email" className="email" placeholder="email@email.com" value={email} onChange={(e)=> setEmail(e.target.value)} /><br/>
-      <input type="password" name="senha" className="senha" placeholder="Digite uma senha" value={senha} onChange={(e)=> setSenha(e.target.value)} /><br/>
-      <button type="button" onClick={CadastrarUsuario}>Cadastrar</button>
+    <div className="conteudoSignup">
+      <div className="formSignup">
+        <h1>Cadastre-se</h1>
+        
+        <input type="text" name="nome" className="nome" placeholder="Insira seu nome" value={nome} onChange={(e)=> setNome(e.target.value)} /><br/>
+        <input type="email" name="email" className="email" placeholder="email@email.com" value={email} onChange={(e)=> setEmail(e.target.value)} /><br/>
+        <input type="password" name="senha" className="senha" placeholder="Digite uma senha" value={senha} onChange={(e)=> setSenha(e.target.value)} /><br/>
+        <button type="button" onClick={CadastrarUsuario}>Cadastrar</button><br/>
+        <p>Já tem uma conta? <a href="/signin">Clique aqui</a></p>
 
-      {(localStorage.getItem("token") !== undefined && localStorage.getItem("token") !== null) &&
-      <div>
-        <p>Usuário logado com sucesso!</p>
       </div>
-      }
       
     </div>
   )
